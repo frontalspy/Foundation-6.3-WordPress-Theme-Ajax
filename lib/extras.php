2b662cea488e87d4afaa0e374ae8da6ec15254eb
frontalspy <frontalspy@users.noreply.github.com>
2017-03-01 12:05:13 +1100

    functions om> 1566348042 +1000
    Formatting

--- lib/extras.php
+++ lib/extras.php
@@ -6,44 +6,32 @@ if (!defined('ABSPATH')) {
   exit; // Exit if accessed directly
 }
 
-
 use Roots\Sage\Setup;
 
 /**
  * Add <body> classes
  */
-function body_class($classes) 
-{
-    // Add page slug if it doesn't exist
-    if (is_single() || is_page() && !is_front_page()) {
-        if (!in_array(basename(get_permalink()), $classes)) {
-            $classes[] = basename(get_permalink());
-        }
+function body_class($classes) {
+  // Add page slug if it doesn't exist
+  if (is_single() || is_page() && !is_front_page()) {
+    if (!in_array(basename(get_permalink()), $classes)) {
+      $classes[] = basename(get_permalink());
     }
+  }
 
-    // Add class if sidebar is active
-    if (Setup\display_sidebar()) {
-        $classes[] = 'sidebar-primary';
-    }
+  // Add class if sidebar is active
+  if (Setup\display_sidebar()) {
+    $classes[] = 'sidebar-primary';
+  }
 
-    return $classes;
+  return $classes;
 }
 add_filter('body_class', __NAMESPACE__ . '\\body_class');
 
 /**
  * Clean up the_excerpt()
  */
-function excerpt_more() 
-{
-    return ' &hellip; <a href="' . get_permalink() . '">' . __('Continued', 'sage') . '</a>';
+function excerpt_more() {
+  return ' &hellip; <a href="' . get_permalink() . '">' . __('Continued', 'sage') . '</a>';
 }
 add_filter('excerpt_more', __NAMESPACE__ . '\\excerpt_more');
-
-class foundationNavMenu extends \Walker_Nav_Menu
-{
-    function start_lvl(&$output, $depth = 0, $args = Array() ) 
-    {
-        $indent = str_repeat("\t", $depth);
-        $output .= "\n$indent<ul class=\"menu\">\n";
-    }
-}
