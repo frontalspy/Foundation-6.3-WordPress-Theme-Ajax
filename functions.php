0c2f74fe8f8c80e8e2ff05b78dd889f27452ab9b
frontalspy <frontalspy@users.noreply.github.com>
2017-05-24 11:49:39 +1000

    Time to hare

--- functions.php
+++ functions.php
@@ -43,9 +43,7 @@ function nonRenderBlockScript($tag, $handle) {
 
 add_filter('script_loader_tag', 'nonRenderBlockScript', 10, 2);
 
-/* 
-
-Recommeded if using Advanced Custom Fields
+set_post_thumbnail_size( 380, 214, array( 'center', 'center') );
 
 //add a options page in the admin panel
 if (function_exists('acf_add_options_page') ) {
@@ -57,9 +55,7 @@ if (function_exists('acf_add_options_page') ) {
       'position' => 'false',
       'redirect' => 'false',    
     )));
-} */
-
-
+}
 // Return this for ACF Images
 function responsiveImage($image_id,$image_size,$max_width){
   $resp = "";
@@ -74,4 +70,42 @@ function responsiveImage($image_id,$image_size,$max_width){
 	$resp = 'data-original="'.$image_src.'" srcset="'.$image_srcset.'" sizes="(max-width: '.$max_width.') 100vw, '.$max_width.'"';
   }
   return $resp;
+}
+
+
+function pagination($pages = '', $range = 2)
+{  
+     $showitems = ($range * 2)+1;  
+
+     global $paged;
+     if(empty($paged)) $paged = 1;
+
+     if($pages == '')
+     {
+         global $wp_query;
+         $pages = $wp_query->max_num_pages;
+         if(!$pages)
+         {
+             $pages = 1;
+         }
+     }   
+
+     if(1 != $pages)
+     {
+         echo "<ul class='pagination'>";
+         if($paged > 2 && $paged > $range+1 && $showitems < $pages) echo "<li><a href='".get_pagenum_link(1)."'>&laquo;</a></li>";
+         if($paged > 1 && $showitems < $pages) echo "<li><a href='".get_pagenum_link($paged - 1)."'>&lsaquo;</a></li>";
+
+         for ($i=1; $i <= $pages; $i++)
+         {
+             if (1 != $pages &&( !($i >= $paged+$range+1 || $i <= $paged-$range-1) || $pages <= $showitems ))
+             {
+                 echo ($paged == $i)? "<span class='current'>".$i."</span>":"<li><a href='".get_pagenum_link($i)."' class='inactive' >".$i."</a></li>";
+             }
+         }
+
+         if ($paged < $pages && $showitems < $pages) echo "<li><a href='".get_pagenum_link($paged + 1)."'>&rsaquo;</a></li>";  
+         if ($paged < $pages-1 &&  $paged+$range-1 < $pages && $showitems < $pages) echo "<li><a href='".get_pagenum_link($pages)."'>&raquo;</a></li>";
+         echo "</ul>\n";
+     }
 }
