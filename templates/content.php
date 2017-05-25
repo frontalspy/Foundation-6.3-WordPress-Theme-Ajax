3b703ca9d61a444bede2b77a8fdc8fab198b7f92
frontalspy <frontalspy@users.noreply.github.com>
2017-05-25 16:51:34 +1000

    Loading assets

--- templates/content.php
+++ templates/content.php
@@ -2,7 +2,7 @@
     exit; // Exit if accessed directly
 } ?>
 
-<article <?php post_class('column small-12 medium-6 large-3'); ?>>
+<article <?php post_class('column small-12 medium-6 large-4'); ?>>
   <div class="content">
     <header>
       <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
