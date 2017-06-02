fbd2a431462ac6d42da1abb3c6f554742851103a
frontalspy <frontalspy@users.noreply.github.com>
2017-06-02 11:12:28 +1000

    MAL Connection

---
+++ dist/scripts/mal-connect-84fec54ceb.js
@@ -0,0 +1,1 @@
+jQuery(document).ready(function(){jQuery(".search-complete input").autocomplete({source:"../wp-content/themes/Foundation-Sage/templates/mal-connect.php",minLength:2,select:function(e,t){jQuery(".search-title input").val(t.item.label),jQuery(".search-output input").val(t.item.score)}})});
