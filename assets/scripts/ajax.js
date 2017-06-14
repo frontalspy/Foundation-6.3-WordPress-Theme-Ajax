450c69db931070a915730111530cdb4baea96f09
frontalspy <frontalspy@users.noreply.github.com>
2017-06-14 16:40:14 +1000

    Whitespacing fixes

--- assets/scripts/ajax.js
+++ assets/scripts/ajax.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function($) {
- 
+
   // If the html content has a h1 tag use this as the title
   function checkTitle(elem) {
     if ($(elem).html()) {
@@ -8,29 +8,41 @@ jQuery(document).ready(function($) {
       document.title = $('title').html();
     }
   }
- 
+
   // Ajax call to get a script and cache it
   $.cachedScript = function(url, options) {
-    options = $.extend( options || {}, {
+    options = $.extend(options || {}, {
       dataType: 'script',
       cache: true,
       url: url
     });
     return jQuery.ajax(options);
   };
- 
+
   // Main bulk of ajax call depends on if browser supports history api.
   // Use Modernizr to check
-  if(Modernizr.history) {
-   
+  if (Modernizr.history) {
+
     // Ajax load pages on all inks other than the WPAdmin Bar
     $(document).on('click','a',  function(e) {
       // Get the URL from the href attribute for the ajax call
       var url = $(this).attr('href');
-      
-      // Only Ajax load if the link is internal
-      if ($(this)[0].host === window.location.host && url.indexOf('wp-admin') < 0 && $(this).attr('target') !== '_blank') {
-        
+
+      // Check if the link is on the same domain/host
+      // If not, handle proper click action
+      var sameHost = Boolean($(this)[0].host === window.location.host);
+
+      // Check if the link redirects to wp-admin
+      //  If not, handle proper click action
+      var ifAdmin = Boolean(url.indexOf('wp-admin') < 0);
+
+      // Check if the link opens in a new tab/window
+      // If yes, handle proper click action
+      var newWindow = Boolean($(this).attr('target') !== '_blank');
+
+      // Only Ajax load if the links are internal
+      if (sameHost && ifAdmin && newWindow) {
+
         // Prevent the default click acion
         e.preventDefault();
         $.ajax({
@@ -46,35 +58,37 @@ jQuery(document).ready(function($) {
           beforeSend: function() {
               // When the ajax call begins, start the loading bar animation
               $('#loader .progress-meter').animate({width:'40%'}, 2000);
-           }
+            }
         })
         .done(function(data) {
           var regx = '(?:.+\/)([^#?]+)';
           var postSlug = url.match(regx)[1].split('/')[0];
           // When the ajax returns a 200 progress the animation          
           $('#loader .progress-meter').animate({width:'75%'}, 3000);
-         
+
           // Replace the content of the main element with the main from the get_post_details
           //$('main.main').html(data);
           $('main.main').html(data);
-         
+
           // Scroll the window to the top of the page
           $(window).scrollTop(0);
-         
+
           // Using the history api, set the current state and url to the new page
           window.history.pushState(data, 'Title', url);
-         
+
           // Change the title
           checkTitle($('h1'));
-         
+
           // Re-initialise Foundation Equalizer for equal height
           Foundation.reInit($('main.main'));
-         
+
           // Finish the loading animation once the ajax has finished loading
-          $('#loader .progress-meter').animate({width:'100%'}).finish().fadeOut('fast', function() {
+          $('#loader .progress-meter').animate({
+            width:'100%'
+          }).finish().fadeOut('fast', function() {
             $(this).css({width:'0%'});
           }).fadeIn('fast');
-         
+
           if ($('#wpadminbar').length) {
             $.ajax({
                   type: 'GET',
@@ -95,13 +109,13 @@ jQuery(document).ready(function($) {
         });
       }
     });
-   
+
     // Ajax loading for search submit
     $(document).on('submit','form.site-search',  function(e) {
-      
+
       // Get the value from the input
       var search = $(this).find('input').val();
-      
+
       // Get the action url and append the wordpress search formatting
       var url = $(this).attr('action')  + 'search/' + search;
       e.preventDefault();
@@ -120,28 +134,28 @@ jQuery(document).ready(function($) {
       .done(function(data) {
         // Replace the content of the main element with the main from the ajax_search
         $('main.main').html(data);
-       
+
         // Using the history api, set the current state and url to the new page
         window.history.pushState(data, "Title", url);
-       
+
         // Check Title
         checkTitle($('h1'));
-       
+
         // Re-initialise Foundation Equalizer for equal height
         Foundation.reInit($('main.main'));
       });
     });
-    
+
     // When trying to change page states (bakwards or forwards) use the history api
     $(window).on('popstate', function(e) {
-      
+
       // Call the previous/next state
       var state = e.originalEvent.state;
       if (state !== null) {
-        
+
         // Replace the content of the main element with the main from the called state
         $('main.main').html(state);
-        
+
         // Check Title
         checkTitle($('h1'));
       }
